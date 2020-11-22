ca82b8cd88086fe1649bd08bf7103608696a1e49
Bryan Ji <sheepjxx@qq.com>
2020-11-22 13:26:40 +0800

    infinite random add questions

--- main.go
+++ main.go
@@ -2,11 +2,35 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"strings"
+	"math/rand"
+	"strconv"
+	"time"
 )
 
+func randomAddQuestion() {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	var a int
+	var b int
+	var input int
+	var result int
+	var promote string
+	a = r.Intn(10)
+	b = r.Intn(10)
+	result = a + b
+	promote = strconv.Itoa(a) + "+" + strconv.Itoa(b) + "="
+	fmt.Print(promote)
+	fmt.Scanln(&input)
+	if input == result {
+		fmt.Println("correct")
+	} else {
+		fmt.Printf("Wrong, correct answer is %d\n", result)
+	}
+}
+
 func main() {
-	fmt.Println(strings.Join(os.Args[1:], " "))
-	fmt.Println(os.Args[0])
+
+	for {
+		randomAddQuestion()
+		time.Sleep(1 * time.Second)
+	}
 }
